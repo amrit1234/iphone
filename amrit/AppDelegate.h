dc17c1b40295730857117d723d41d2c80e5997f1
dharmendrabaghel <dharmendra.syscraft@gmail.com>
2015-02-14 11:30:07 +0530

    Initial Commit

---
+++ amrit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  amrit
+//
+//  Created by inderjit singh on 14/02/15.
+//  Copyright (c) 2015 inderjit singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ amrit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  amrit
+//
+//  Created by inderjit singh on 14/02/15.
+//  Copyright (c) 2015 inderjit singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
